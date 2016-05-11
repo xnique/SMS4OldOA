ae251874e6628d06fe1a2ca0abff868bced6b3d3
xnique <dingwh312@outlook.com>
2016-05-11 23:43:03 +0800

    Baseline

---
+++ SMS4OldOA/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace SMS4OldOA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button6 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(885, 453);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 512);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "测试发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(445, 512);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "检查发送情况";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(38, 503);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "启动";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(134, 503);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(84, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "退出";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(2, 461);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(885, 29);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(550, 514);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(216, 21);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(772, 514);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(115, 21);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(39, 552);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(156, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "设置发送间隔（毫秒）";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(201, 554);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 21);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(389, 551);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "设置扫描间隔（秒）";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(524, 553);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(45, 21);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 588);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SMS4oldOA";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Form1_Scroll);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ SMS4OldOA/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OracleClient;
+using System.Threading;
+using System.Globalization;
+
+namespace SMS4OldOA
+{
+    public partial class Form1 : Form
+    {
+        private TouchSMS.SmsMessageServiceClient SMSC = null;
+        private string username = "smsoldoa";
+        private string password = "Sjiaow*6638oa";
+        private static bool quit = true;
+        private static string oracle_conn = "server=12.5.37.98;Data Source=orcl190;User Id=shoa;Password=Sjiaow_ora#32226638;Integrated Security=no";
+        private OracleConnection conn = new OracleConnection(oracle_conn);
+        private OracleCommand cmd = new OracleCommand();
+        private OracleCommand cmd1 = new OracleCommand();
+        private int sms_id = 0;
+        private string cell_no = "";
+        private string content = "";
+        private static int sendinterval = 200;
+        private static int scaninterval = 3;
+        //private string send_date = "";
+        //private CultureInfo CI = Thread.CurrentThread.CurrentCulture;
+
+        private delegate void SetTextBoxDelegate(string text,string text2);
+        public Form1()
+        {
+            InitializeComponent();
+            button3.Enabled = quit;
+            button4.Enabled = !quit;
+            
+            cmd.CommandText = "select sms_id,content,cell_no from tbl_ad_sms where sms_id>20886182 and scan_flag is null";
+            
+            //cmd1.CommandText = 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TouchSMS.SmsMessage msg = new TouchSMS.SmsMessage();
+            msg.destAddr = "18116286599";
+            msg.content = "oa测试短信";
+            string resp = SMSC.send(username, password, msg);
+            textBox1.AppendText(resp + "\r\n");
+            //SMSC.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TouchSMS.SmsReport[] reports = SMSC.getSmsReports(username, password, textBox3.Text, textBox4.Text);
+            if (reports.Length>0) textBox1.AppendText(reports[0].statusCode.ToString() + "\r\n");
+            //textBox1.Text = DateTime.Now.ToString();
+            
+        }
+
+        private void setTextBox(string text,string text2)
+        {
+            if (!this.InvokeRequired)
+            {
+                if (text != "") textBox1.AppendText(text + "\r\n");
+                if (text2 != "") textBox2.Text = text2;
+            }
+            else
+            {
+                SetTextBoxDelegate stbd = new SetTextBoxDelegate(setTextBox);
+                this.BeginInvoke(stbd, text,text2);
+            }
+        }
+
+
+
+        private void SendSMS()
+        {
+            while(true)
+            {
+                try
+                {
+                    if (quit) break;
+                    if (conn.State != ConnectionState.Open) conn.Open();
+                    OracleDataReader odr = cmd.ExecuteReader();
+                    setTextBox("", "cmd executed..." + DateTime.Now.ToString());
+                    while (odr.Read())
+                    {
+                        sms_id = odr.GetInt32(0);
+                        content = odr.GetString(1);
+                        cell_no = odr.GetString(2);
+                        TouchSMS.SmsMessage msg = new TouchSMS.SmsMessage();
+                        msg.destAddr = cell_no;
+                        msg.content = content;
+                        string resp = SMSC.send(username, password, msg);
+                        setTextBox(DateTime.Now.ToString() + "，向" + cell_no + "发送短信，内容为：【" + content + "】。发送结果ID" + resp, "");
+                        cmd1.CommandText = "update tbl_ad_sms set scan_flag=1,status=1,send_date=to_date('" + DateTime.Now.ToString() + "','yyyy/mm/dd hh24:mi:ss') where sms_id=" + sms_id.ToString();
+                        cmd1.ExecuteNonQuery();
+                        System.Threading.Thread.Sleep(sendinterval);
+                    }
+                    odr.Close();
+                    System.Threading.Thread.Sleep(scaninterval * 1000);
+                }
+                catch(Exception ex)
+                {
+                    setTextBox(ex.ToString(), "");
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (SMSC == null) SMSC = new TouchSMS.SmsMessageServiceClient();
+            if (conn.State != ConnectionState.Open)
+            {
+                try
+                {
+                    conn.Open();
+                    quit = false;
+                    Thread t = new Thread(SendSMS);
+                    t.Start();
+                    button3.Enabled = quit;
+                    button4.Enabled = !quit;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("连接数据库失败！" + ex.ToString());
+                    button3.Enabled = false;
+                    quit = true;
+                }
+                cmd.Connection = conn;
+                cmd1.Connection = conn;
+            }
+            else
+            {
+                quit = false;
+                Thread t = new Thread(SendSMS);
+                t.Start();
+                button3.Enabled = quit;
+                button4.Enabled = !quit;
+            }
+            //Thread.Sleep(10000);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            quit = true;
+            button3.Enabled = quit;
+            button4.Enabled = !quit;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            quit = true;
+            conn.Close();
+        }
+
+        private void Form1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sendinterval = (int)numericUpDown1.Value;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            scaninterval = (int)numericUpDown2.Value;
+        }
+    }
+}
